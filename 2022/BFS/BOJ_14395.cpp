592868749dfabcf9cf904ff44a46cf12a968f62b
youngtae6302@naver.com <youngtae6302@naver.com>
2022-10-08 17:28:22 +0900

    BOJ_14395-ing

---
+++ 2022/BFS/BOJ_14395.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int s, t;
+
+
+char op[4] = { '*','+','-','/' };
+
+
+int Func(int sum) {
+
+	if (sum == t) {
+
+	}
+
+
+}
+
+
+int main() {
+
+	cin >> s >> t;
+
+
+	Func(s);
+
+
+	return 0;
+}
