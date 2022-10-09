1333f9fe785502779311854b3eee0e4afe0c4706
youngtae6302@naver.com <youngtae6302@naver.com>
2022-10-09 23:11:34 +0900

    BFS_4op

--- 2022/BFS/BOJ_14395.cpp
+++ 2022/BFS/BOJ_14395.cpp
@@ -1,3 +1,5 @@
+// BOJ 14395
+
 #include <iostream>
 #include <queue>
 #include <stack>
@@ -5,27 +7,46 @@ using namespace std;
 
 int s, t;
 
+int minus, div;
 
-char op[4] = { '*','+','-','/' };
-
-
-int Func(int sum) {
-
-	if (sum == t) {
-
-	}
-
-
-}
-
+struct op {
+	unsigned long long num;
+	string ans;
+};
 
 int main() {
 
 	cin >> s >> t;
 
+	queue<op> q;
+	q.push({s,""});
+	while (!q.empty()) {
+		unsigned long long a = q.front().num;
+		string temp = q.front().ans;
+		q.pop();
 
-	Func(s);
-
+		if (a == t) {
+			if(temp == "")
+				cout << 0 << '\n';
+			else
+				cout << temp << '\n';
+			return 0;
+		}
+		if (a * a <= t && a * a > 1);
+			q.push({a*a,temp+'*'});
+		if(a+a<=t&&a+a>0)
+			q.push({ a + a,temp + '+' });
+		if (minus == 0) {
+			q.push({1,temp+'/'});
+			div=1;
+		}
+		if (div = 1) {
+			q.push({1,temp+'/'});
+			div = 1;
+		}
+			
+	}
+	cout << -1 << endl;
 
 	return 0;
 }
