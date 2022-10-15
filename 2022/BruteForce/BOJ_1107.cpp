431f4ba8b8e1d027abf7535db771b9359c06ebd6
youngtae6302@naver.com <youngtae6302@naver.com>
2022-10-15 22:23:26 +0900

    BruteForce_Remotecon

---
+++ 2022/BruteForce/BOJ_1107.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int channel;
+int n;
+int button[10];
+
+int main() {
+
+	cin >> channel >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> button[i];
+	}
+
+
+	return 0;
+}
